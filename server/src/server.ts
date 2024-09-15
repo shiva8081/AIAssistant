9c71abe8cf8a5df75be0735334a407907b0f8477
shiva8081 <yadavshiva8755@gmail.com>
2024-09-15 22:56:49 +0530

    setup

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+
+
+const app=express();
+
+const port=5000;
+app.listen(port,()=>{
+    console.log(`running on port ${port}`)
+})
